3e74c03e0ff5fdc02c7335ab59079099831cf4b0
aadityagi21 <adityamcu1245@gmail.com>
2025-11-11 18:48:31 +0530

    Create learn.py

---
+++ learn.py
@@ -0,0 +1,34 @@
+# n=5
+# X="MADAM"
+# for i in range(0,n):
+#     for j in range(0,n):
+#         if(i==0 or i==n-1 or i==j or i+j==n-1):print(X[j],end="     ")
+#         else:print(end="      ")
+#     print()
+
+
+# for i in range(0,5):
+#     for j in range(0,i+1):
+#         print("*",end=" ")
+#     print()
+
+# for i in range(0,5):print("* "*(5-i))
+
+# for i in range(5):
+#     for j in range(5-i-1):
+#         print(" ",end=" ")
+#     for j in range(i+1):
+#         print("* ",end="")
+#     print()
+    
+# for i in range(5):
+#     print(" "*(5-i-1)+"*"*(i+1))
+    
+# for i in range(5):
+# #     print(" "*(i)+"*"*(5-i))
+
+# for i in range(5): print(" "*(i)+"*"*(5-i)+"*"*(5-i)+" "*(i))
+# for i in range(5): print(" "*(5-i-1)+"*"*(i+1)+"*"*(i+1)+" "*(5-i))
+
+x="hi"
+print(x[2:-1:-1])
